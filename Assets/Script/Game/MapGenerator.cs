286b574e10330a42d862beafbb406a5c896cc2a4
LSH005 <isihyeon005shingu@gmail.com>
2025-12-15 19:55:37 +0900

    [feat] 지형 생성기 구현

---
+++ Assets/Script/Game/MapGenerator.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    [Header("지형")]
+    public GameObject terrain;
+
+    [Header("지형 범위")]
+    public int width = 20;
+    public int depth = 20;
+    public int terrainGap;
+    public int maxHeight = 6;
+
+    [Header("노이즈")]
+    public float noiseScale = 20f;
+
+    void Start()
+    {
+        float offsetX = Random.Range(-9999f, 9999f);
+        float offsetY = Random.Range(-9999f, 9999f);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < depth; z++)
+            {
+                float nx = (x + offsetX) / noiseScale;
+                float nz = (z + offsetY) / noiseScale;
+
+                float noise = Mathf.PerlinNoise(nx, nz);
+                int y = Mathf.FloorToInt(noise * maxHeight);
+
+                SetBlock(x, y, z);
+            }
+        }
+
+    }
+
+    void SetBlock(int x, int y, int z)
+    {
+        x *= terrainGap; z *= terrainGap;
+
+        var go = Instantiate(terrain, new Vector3(x, y, z), Quaternion.identity, transform);
+        go.name = $"Terrain : {x} // {y} // {z}";
+        go.transform.SetParent(transform);
+    }
+}
